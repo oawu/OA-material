89ea8810de68601a72fd4e826e7996d65608656c
OA Wu <comdan66@gmail.com>
2014-12-14 19:05:46 +0800

    Temp

--- compass/config.rb
+++ compass/config.rb
@@ -3,7 +3,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/OA-material"
 css_dir = "../css/"
 sass_dir = "../scss/"
 
