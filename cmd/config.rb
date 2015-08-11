bd89133bb5fbedae8a82bfa4a9b639514bf50164
OA Wu <comdan66@gmail.com>
2015-08-11 23:12:12 +0800

    Dev Temp

--- cmd/config.rb
+++ cmd/config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 
-http_path = "/f2e"
+http_path = "/OA-material"
 
 fonts_dir = "font"
 fonts_path = "../font"
