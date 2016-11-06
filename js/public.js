2a194fa641aedc1034961ed971c84e4138422c03
OA Wu <comdan66@gmail.com>
2016-11-06 19:27:59 +0800

    Fix code.

--- js/public.js
+++ js/public.js
@@ -7,9 +7,9 @@
 var ENVIRONMENT = 'production';
 
 if (ENVIRONMENT == 'dev') {
-  window.url = 'https://dev.works.ioa.tw/OA-material/index.html';
+  window.url = 'https://dev.works.ioa.tw/OA-material/';
 } else {
-  window.url = 'https://works.ioa.tw/OA-material/index.html';
+  window.url = 'https://works.ioa.tw/OA-material/';
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
